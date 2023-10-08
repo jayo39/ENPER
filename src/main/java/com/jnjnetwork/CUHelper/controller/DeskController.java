3db3c5ce421313d0386edd6392f5a921221d1527
jayo39 <sjeaho@gmail.com>
2023-10-08 19:15:34 +0900

    rename project, add enroll functionality

---
+++ src/main/java/com/jnjnetwork/CUHelper/controller/DeskController.java
@@ -0,0 +1,14 @@
+package com.jnjnetwork.CUHelper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/desk")
+public class DeskController {
+
+    @GetMapping("/enroll")
+    public void enroll() {;}
+
+}
---
+++ src/main/resources/static/js/enroll.js
@@ -0,0 +1,57 @@
+let mainBox = $('.main-box');
+let unenroll = $('#unenroll');
+let enroll = $('#enroll');
+
+function getResult() {
+    let yesterdayInput = $('#yesterday').val();
+    let todayInput = $('#today').val();
+    let tomorrowInput = $('#tomorrow').val();
+
+    let cleanedYesterday = processInput(yesterdayInput);
+    let cleanedToday = processInput(todayInput);
+    let cleanedTomorrow = processInput(tomorrowInput);
+
+    let unenrollList = getUnenroll(cleanedYesterday, cleanedToday, cleanedTomorrow);
+    let enrollList = getEnroll(cleanedYesterday, cleanedToday, cleanedTomorrow);
+
+    mainBox.removeClass('d-none');
+
+    if(unenrollList.length === 0) {
+        unenroll.text('None');
+    } else {
+        unenroll.text(unenrollList);
+    }
+
+    if (enrollList.length === 0) {
+        enroll.text('None');
+    } else {
+        enroll.text(enrollList);
+    }
+
+}
+
+function processInput(input) {
+    input = input.replace(/\([^)]*\)/g, '');
+
+    let array = input.split(/[\/,\n]/);
+
+    array = array.map(item => item.replace(/^\d+/, '').trim()).filter(item => item !== '');
+
+    return array;
+  }
+
+function getUnenroll(yesterdayArray, todayArray, tomorrowArray) {
+    const combinedSet = new Set([...todayArray, ...tomorrowArray]);
+
+    const newArray = yesterdayArray.filter(item => !combinedSet.has(item));
+
+    return newArray;
+}
+
+function getEnroll(yesterdayArray, todayArray, tomorrowArray) {
+    const combinedSet = new Set([...yesterdayArray, ...todayArray]);
+
+    const newArray = tomorrowArray.filter(item => !combinedSet.has(item));
+
+    return newArray;
+}
