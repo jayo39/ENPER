8ae5111d295cfca90a0588c8f3002df9dfe40367
jayo39 <sjeaho@gmail.com>
2023-11-14 15:47:09 +0900

    add favicon

--- src/main/java/com/jnjnetwork/CUHelper/domain/User.java
+++ src/main/java/com/jnjnetwork/CUHelper/domain/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
