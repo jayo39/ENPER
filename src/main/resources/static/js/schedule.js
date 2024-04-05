84faa35491027e08354268d55da8ee21069f223f
jayo39 <sjeaho@gmail.com>
2024-04-05 16:56:12 +0900

    add custom timepicker

--- src/main/resources/static/js/schedule.js
+++ src/main/resources/static/js/schedule.js
@@ -1,3 +1,20 @@
+const clockmodal = $('#clock-modal');
+const hourButton = document.querySelector('.timepicker-hour');
+const minuteButton = document.querySelector('.timepicker-minute');
+const clockFaceHour = document.querySelector('#hour .timepicker-clock');
+const clockFaceMinute = document.querySelector('#minute .timepicker-clock');
+const timepickerHandHour = document.querySelector('#handHour');
+const timepickerHandMin = document.querySelector('#handMin');
+const hourMarkers = document.querySelectorAll('.timepicker-time-tips-hours');
+const minuteMarkers = document.querySelectorAll('.timepicker-time-tips-minutes');
+const hourView = document.querySelector('#hour');
+const minuteView = document.querySelector('#minute');
+let isDragging = false;
+let mode = 'hour';
+let lastClickedTimeInput = null;
+let savehour = null;
+let saveminute = null;
+
 $(function() {
     const sidebar = $('#sidebar');
     const openSidebar = $('#schedule');
@@ -8,6 +25,16 @@ $(function() {
     let bodyWidth = body.width();
 
     $("#sidebar").removeClass('d-none');
+
+    clockFaceHour.addEventListener('mousedown', handleStart);
+    clockFaceMinute.addEventListener('mousedown', handleStart);
+    document.addEventListener('mousemove', handleMove);
+    document.addEventListener('mouseup', handleEnd);
+
+    clockFaceHour.addEventListener('touchstart', handleStart);
+    clockFaceMinute.addEventListener('touchstart', handleStart);
+    document.addEventListener('touchmove', handleMove, { passive: false });
+    document.addEventListener('touchend', handleEnd);
 
     if(!(typeof scheUser_id === 'undefined') && localStorage.getItem('barStatus') === 'open') {
         sidebar.css('transition', 'none');
@@ -97,6 +124,8 @@ $(function() {
         });
         $('#schedule-studentName').val('');
         $('#schedule-endTime-initial').val('');
+        lastClickedTimeInput.getElementsByClassName("display-time")[0].textContent = '';
+        lastClickedTimeInput = null;
     });
 });
 
@@ -127,6 +156,14 @@ function buildSchedule(result) {
         let endTime = schedule.endTime;
         let content = schedule.note === null ? "" : schedule.note;
         let isFinished = schedule.isFinished;
+        let [hours, minutes] = endTime.split(':').map(num => parseInt(num, 10));
+
+        let ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+
+        let newTime = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ' ' + ampm;
+
         const row = `
             <div id="schedule-${id}">
                 <div id="schedule-info-${id}" class="d-flex justify-content-between align-items-center text-yellow input-group">
@@ -134,7 +171,13 @@ function buildSchedule(result) {
                       <div data-schedule-id-name="${id}" class="studentName">${studentName}</div>
                     </div>
                     <div class="mb-1 ms-1">
-                      <input data-schedule-id-time="${id}" type="time" class="form-control time-input top-input" id="schedule-endTime-${id}" placeholder="" value="${endTime}">
+                      <div class="top-input form-control time-input d-flex justify-content-end align-items-center column-gap-3">
+                        <div class="display-time">${newTime}</div>
+                        <div>
+                          <i class="far fa-clock"></i>
+                        </div>
+                      </div>
+                      <input data-schedule-id-time="${id}" type="time" class="form-control" id="schedule-endTime-${id}" value="" hidden>
                     </div>
                 </div>
             </div>
@@ -329,4 +372,224 @@ function checkSession(successCallback) {
             location.href = '/user/login';
         }
     });
+}
+
+function openClockModal() {
+    clockmodal.removeClass('d-none');
+    $('.timepicker-hour').addClass('active');
+    $('.timepicker-minute').removeClass('active');
+}
+
+function closeClockModal() {
+    clockmodal.addClass('d-none');
+}
+
+$(document).on('click', '.time-input', function() {
+    lastClickedTimeInput = this;
+    mode = 'hour';
+    if (lastClickedTimeInput.getElementsByClassName("display-time")[0].textContent) {
+        let [time, period] = lastClickedTimeInput.getElementsByClassName("display-time")[0].textContent.split(' ');
+        [savehour, saveminute] = time.split(':');
+
+        hourButton.textContent = savehour;
+        minuteButton.textContent = saveminute;
+
+        savehour = parseInt(savehour);
+        saveminute = parseInt(saveminute);
+
+        updateActiveMarker(Math.floor(savehour % 12), 'hour');
+        if (period === 'AM') {
+            $('.timepicker-am').addClass('active');
+            $('.timepicker-pm').removeClass('active');
+        } else {
+            $('.timepicker-pm').addClass('active');
+            $('.timepicker-am').removeClass('active');
+        }
+
+        hourView.classList.remove('d-none');
+        minuteView.classList.add('d-none');
+        const hourDegrees = ((savehour % 12) / 12) * 360;
+        setHandRotation(hourDegrees, 'hour');
+    } else {
+        resetClock();
+    }
+    openClockModal();
+});
+
+$(document).on('click', '.timepicker-cancel', function() {
+    closeClockModal();
+});
+
+$(document).on('click', '.timepicker-clear', function() {
+    resetClock();
+});
+
+$(document).on('click', '.timepicker-am', function() {
+    document.querySelector('.timepicker-am').classList.add('active');
+    document.querySelector('.timepicker-pm').classList.remove('active');
+});
+
+$(document).on('click', '.timepicker-pm', function() {
+    document.querySelector('.timepicker-pm').classList.add('active');
+    document.querySelector('.timepicker-am').classList.remove('active');
+});
+
+$(document).on('click', '.timepicker-hour', function() {
+    toggleToHourSelection();
+});
+
+$(document).on('click', '.timepicker-minute', function() {
+    toggleToMinuteSelection();
+});
+
+$(document).on('click', '.timepicker-button.timepicker-submit', function() {
+    const newhour = hourButton.textContent;
+    const newminute = minuteButton.textContent;
+    const amPm = document.querySelector('.timepicker-hour-mode.active').textContent;
+
+    lastClickedTimeInput.getElementsByClassName("display-time")[0].textContent = `${newhour}:${newminute} ${amPm}`;
+
+    const timeForDatabase = convertTo24HourFormat(parseInt(newhour, 10), newminute, amPm);
+    if (lastClickedTimeInput.getAttribute('id') === "initial") {
+        document.querySelector('#schedule-endTime-initial').value = timeForDatabase;
+    } else {
+        lastClickedTimeInput.nextSibling.nextSibling.value = timeForDatabase;
+        let event = new Event('change', {
+            'bubbles': true,
+            'cancelable': true
+        });
+        lastClickedTimeInput.nextSibling.nextSibling.dispatchEvent(event);
+    }
+    closeClockModal();
+});
+
+
+function toggleToMinuteSelection() {
+    $('#hour').addClass('d-none');
+    $('#minute').removeClass('d-none');
+    $('.timepicker-hour').removeClass('active');
+    $('.timepicker-minute').addClass('active');
+    mode = 'minute';
+    if (saveminute) {
+        const minuteDegrees = saveminute * 6;
+        updateActiveMarker(saveminute / 5, 'minute');
+        setHandRotation(minuteDegrees, 'minute');
+    } else {
+        updateActiveMarker(0, 'minute');
+        setHandRotation(0, 'minute');
+    }
+}
+
+function toggleToHourSelection() {
+    $('#minute').addClass('d-none');
+    $('#hour').removeClass('d-none');
+    $('.timepicker-minute').removeClass('active');
+    $('.timepicker-hour').addClass('active');
+    mode = 'hour';
+    if (savehour) {
+        updateActiveMarker(Math.floor(savehour % 12), 'hour');
+        const hourDegrees = ((savehour % 12) / 12) * 360;
+        setHandRotation(hourDegrees, 'hour');
+    } else {
+        updateActiveMarker(0, 'hour');
+        setHandRotation(360, 'hour');
+    }
+}
+
+function snapToNearest(degrees, mode) {
+    return mode === 'hour' ? Math.round(degrees / 30) * 30 : Math.round(degrees / 6) * 6;
+}
+
+function updateActiveMarker(index, mode) {
+    const markers = mode === 'hour' ? hourMarkers : minuteMarkers;
+    markers.forEach((marker, i) => {
+        marker.classList.remove('active');
+        if (i === index) {
+            marker.classList.add('active');
+        }
+    });
+}
+
+function calculateRotation(e) {
+    const target = mode === 'hour' ? clockFaceHour : clockFaceMinute;
+    const rect = target.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    let x = e.clientX;
+    let y = e.clientY;
+
+    if (e.touches) {
+        x = e.touches[0].clientX;
+        y = e.touches[0].clientY;
+    }
+
+    const angleDeg = Math.atan2(y - centerY, x - centerX) * 180 / Math.PI + 90;
+    return angleDeg < 0 ? angleDeg + 360 : angleDeg;
+}
+
+function updateHandAndDisplay(angle) {
+    const snappedAngle = snapToNearest(angle, mode);
+    setHandRotation(snappedAngle, mode);
+    if (mode === 'hour') {
+        const hour = snappedAngle / 30 || 12;
+        hourButton.textContent = hour < 10 ? `0${hour}` : hour;
+        updateActiveMarker(Math.floor(hour % 12), 'hour');
+    } else {
+        const minute = snappedAngle / 6;
+        minuteButton.textContent = minute < 10 ? `0${minute}` : minute;
+        updateActiveMarker(minute / 5, 'minute');
+    }
+}
+
+function setHandRotation(degrees, mode) {
+    if (mode === 'hour') {
+        timepickerHandHour.style.transform = `rotateZ(${degrees}deg)`;
+    } else {
+        timepickerHandMin.style.transform = `rotateZ(${degrees}deg)`;
+    }
+}
+
+function handleStart(e) {
+    e.preventDefault();
+    isDragging = true;
+    const angle = calculateRotation(e);
+    updateHandAndDisplay(angle);
+}
+
+function handleMove(e) {
+    if (!isDragging) return;
+    const angle = calculateRotation(e);
+    updateHandAndDisplay(angle);
+}
+
+function handleEnd() {
+    if (!isDragging) return;
+    isDragging = false;
+    if (mode === 'hour') toggleToMinuteSelection();
+}
+
+function convertTo24HourFormat(hour, minute, amPm) {
+    let hourIn24 = hour % 12;
+    if (amPm === 'PM') {
+        hourIn24 += 12;
+    }
+    return `${hourIn24.toString().padStart(2, '0')}:${minute.padStart(2, '0')}:00`;
+}
+
+function resetClock() {
+    hourButton.textContent = '12';
+    minuteButton.textContent = '00';
+    saveminute = null;
+    document.querySelector('.timepicker-am').classList.remove('active');
+    document.querySelector('.timepicker-pm').classList.add('active');
+
+    setHandRotation(360, 'hour');
+    setHandRotation(0, 'minute');
+
+    $('#hour').removeClass('d-none');
+    $('#minute').addClass('d-none');
+    mode = 'hour';
+
+    updateActiveMarker(0, 'hour');
+    updateActiveMarker(0, 'minute');
 }
