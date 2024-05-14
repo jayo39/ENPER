4d547edf0c2ff86b0f9777ada81de79c92f43b55
jayo39 <sjeaho@gmail.com>
2024-05-14 15:02:04 +0900

    change enroll

--- src/main/resources/static/js/enroll.js
+++ src/main/resources/static/js/enroll.js
@@ -1,38 +1,24 @@
 let mainBox = $('.main-box');
-let unenroll = $('#unenroll');
 let enroll = $('#enroll');
 
 function reset() {
     mainBox.addClass('d-none');
-    document.getElementById('yesterday').value = '';
-    document.getElementById('today').value = '';
     document.getElementById('tomorrow').value = '';
 }
 
 function getResult() {
-    let yesterdayInput = $('#yesterday').val();
-    let todayInput = $('#today').val();
     let tomorrowInput = $('#tomorrow').val();
 
-    let cleanedYesterday = processInput(yesterdayInput);
-    let cleanedToday = processInput(todayInput);
     let cleanedTomorrow = processInput(tomorrowInput);
 
-    let unenrollList = getUnenroll(cleanedYesterday, cleanedToday, cleanedTomorrow);
-    let enrollList = getEnroll(cleanedYesterday, cleanedToday, cleanedTomorrow);
+    let enrollList = cleanedTomorrow;
 
     mainBox.removeClass('d-none');
-
-    if(unenrollList.length === 0) {
-        unenroll.text('None');
-    } else {
-        unenroll.text(unenrollList);
-    }
 
     if (enrollList.length === 0) {
         enroll.text('None');
     } else {
-        enroll.text(enrollList);
+        enroll.text(enrollList.join(', '));
     }
 
 }
@@ -44,17 +30,7 @@ function processInput(input) {
 
     array = array.map(item => item.replace(/^\d+/, '').trim()).filter(item => item !== '');
 
+    array = [...new Set(array)];
+
     return array;
-  }
-
-function getUnenroll(yesterdayArray, todayArray, tomorrowArray) {
-    const combinedSet = new Set([...yesterdayArray, ...todayArray]);
-
-    const newArray = Array.from(combinedSet).filter(item => !tomorrowArray.includes(item));
-
-    return newArray;
-}
-
-function getEnroll(yesterdayArray, todayArray, tomorrowArray) {
-    return tomorrowArray;
-}
+ }
