18a0b7286705da14668d2ba4fe8319fd87f6c10a
jayo39 <sjeaho@gmail.com>
2024-07-21 17:20:57 +0900

    show error msg

--- src/main/java/com/jnjnetwork/CUHelper/controller/CustomErrorController.java
+++ src/main/java/com/jnjnetwork/CUHelper/controller/CustomErrorController.java
@@ -5,13 +5,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object errorMessage = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        model.addAttribute("errorMessage", errorMessage != null ? errorMessage.toString() : "Unknown error");
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
