68851d0bb8cfc9f07f4d0dd4a9b12d819fa93ab6
jayo39 <sjeaho@gmail.com>
2025-07-15 11:44:03 +0900

    better image handling

--- src/main/resources/static/js/booklist.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-   $('.clickable').click(function() {
-        var bookId = $(this).children("input").val();
-        location.href = "/book/detail/" + bookId;
-        th:href="@{'/book/detail/' + ${book.id}}"
-   });
-});
